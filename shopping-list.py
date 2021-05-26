2b4a63e5eb38efcaf74c2f5f47db78335b464457
Krzysztof Lach <lachkrzysztof56@gmail.com>
2021-05-26 07:02:27 +0200

    Add loop and sentence

--- shopping-list.py
+++ shopping-list.py
@@ -1,3 +1,8 @@
 # Zadanie 1 "Wyprawa po zakupy."
 shopping_list = {'piekarnia': ('chleb', 'bułki', 'pączek'), 
                  'warzywniak': ('marchew', 'seler', 'rukola')}
+
+
+# Nazwy sklepów i produktów wypisane wielką literą
+for shop, things in shopping_list.items():
+    print("Idę do {} kupuję tu następujące rzeczy: {}".format(shop.capitalize(), str(things).title()))
